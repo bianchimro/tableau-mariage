7eb205a116b8e4caafdb589a02b26f9f4bffe374
Mauro Bianchi <bianchimro@gmail.com>
2015-05-01 01:18:12 +0200

    first commit

---
+++ tavoli.js
@@ -0,0 +1,80 @@
+var tavoli =  [
+    { nome : "Castagno", 
+      invitati: ["Renato", "Neili", "Tino", "Giuseppina", "Anna", "Silvano", "Angela", "Claudio", "Marco", "Stefania"],
+      img : "immagini/castagno.jpg"
+    },
+
+    { nome : "Quercia", 
+      invitati: ["Maria", "Giulia", "Andrea", "Antonio", "Anna", "Aurora", "Vito", "Aline", "Guillermo"],
+      img : "immagini/quercia.jpg"
+    },
+
+    { nome : "Betulla", 
+      invitati: ["Virgilia", "Zia Emilia", "Zia Angiolina", "Maria Teresa", "Giuseppe", "Cinda", "Marinella", "Luigi"],
+      img : "immagini/betulla.jpg"
+    },
+    { nome : "Baobab", 
+      invitati: ["Max", "Damiana", "Marco", "Mirko", "Carlandrea", "Fede", "Luca", "Cristina", "Tommaso", "Ricky"],
+      img : "immagini/baobab.jpg"
+    },
+
+    { nome : "Faggio", 
+      invitati: ["Valeria", "Matteo", "Oliviero", "Cristina", "Valentina", "Marco", "Dario", "Nicole", "Chiara", "Marco DT"],
+      img : "immagini/faggio.jpg"
+    },
+
+    { nome : "Pioppo", 
+      invitati: [ "Mauro D.", "Marco A.",  "Daniela", "Lele", "Gio", "Marco Gufo", "Matteo G.", "Marcello"],
+      img : "immagini/pioppo.jpg"
+    },
+
+    { nome : "Ciliegio", 
+      invitati: ["Giusi", "Antonio", "Mario", "Virginia", "Gigi", "Sabina", "Giampiero", "Laura", "Elisio"],
+      img : "immagini/ciliegio.jpg"
+    },
+
+    { nome : "Acero", 
+      invitati: ["Marco", "Sefy", "Patrizia", "Oscar", "Piera", "Maria Teresa", "Giancarlo", "Maura"],
+      img : "immagini/acero.jpg"
+    },
+
+    { nome : "Salice", 
+      invitati: ["Simona", "Marco", "Miriam", "Davide", "Filippo", "Martina", "Giulia"],
+      img : "immagini/salice.jpg"
+    },
+
+    { nome : "Edera", 
+      invitati: ["Monica", "Angelo", "Federica", "Andrea", "Stefania", "Lorenzo", "Gabriele"],
+      img : "immagini/edera.jpg"
+
+    },
+
+    { nome : "Ulivo", 
+      invitati: ["Andrea", "Andrea", "Elia" , "Marta", "Adriana", "Sheila", "Luigi", "Clara", "Giusy", "Graziano"],
+      img  : "immagini/ulivo.jpg"
+
+    },
+
+    { nome : "Fico", 
+      invitati: ["Luca", "Daniela", "Simone", "Elisa", "Enrico", "Fra", "Antonio", "Roberta", "Letizia", "Marc", "Philippe"],
+      img : "immagini/fico.jpg"
+    },
+
+    { nome : "Alloro", 
+      invitati: ["Rachele", "Francesco", "Lorenzo", "Emiliano", "Enrico", "Mauro", "Camilla", "Cristina", "Elena"],
+      img : "immagini/alloro.jpg"
+    },
+
+    { nome : "Mirto", 
+      invitati: ["Marianna", "Marco", "Emma", "Jacopo", "Andrea", "Melissa", "Vittoria", "Alex", "Chiara", "Davide"],
+      img : "immagini/mirto.jpg"
+    },
+    {
+      nome:"Eucalipto",
+      invitati : ["Eleonora e Kalidou", "Sara", "Mauro", "Cristina", "Paola", "Dave", "Elisa", "Katia", "Clara e Rocco", "Valeria"],
+      img : "immagini/eucalipto.jpg"
+    } 
+
+
+]
+
